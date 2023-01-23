bbea16d64976d5ac766773b60c4c4c22cda4e079
1 <mperezperez@danielcastelao.org>
2023-01-23 08:50:57 +0100

    Rama camara

--- app/src/main/java/com/example/trimestre2/FirstActivity.kt
+++ app/src/main/java/com/example/trimestre2/FirstActivity.kt
@@ -4,56 +4,40 @@ import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.graphics.Bitmap
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class FirstActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
 
-    val RESULTADO_UNO = 1
-    val RESULTADO_DOS = 2
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //El boton para iniciar la camara
+        val callButton = findViewById<Button>(R.id.button)
 
-        var goSecond = findViewById<Button>(R.id.button)
-
-        goSecond.setOnClickListener{
-            // Crear un Intent para iniciar la segunda acividad
-            val intent = Intent(this, secondActivity::class.java)
-
-            //Añade datos adicionale al Intent
-            intent.putExtra("proveedores","2")
-            intent.putExtra("clientes", 3)
-
-            //Iniciar la segunda actividad
-            startActivityForResult(intent, RESULTADO_UNO)
-
-            startActivityForResult(intent, RESULTADO_DOS)
-
+        callButton.setOnClickListener{
+            //creamos un intent específico que iniciará el teléfono
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            //Llamamos a la activity teléfono
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
         }
 
     }
-    @Deprecated("Deprecated a partir de API 30")
 
-    //recoje el internet cuando hacemos setResult() en la SecondActivity
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode, data)
-        //Definimos el text view para mostrar el dato que nos manda la Second
-
-       val saludo = findViewById<TextView>(R.id.textView3)
-        if(resultCode != Activity.RESULT_OK)return
-        when(requestCode){
-            RESULTADO_UNO -> {
-                //Si el intent no es null muestra el resultado
-                if(data != null) {
-                    saludo.text=data.getStringExtra("saludo")
-                }; }
-            //Other result code
-            else -> {}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && data != null){
+            val imageBitmap = data.extras !!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
         }
     }
+
+
 
 }
 
